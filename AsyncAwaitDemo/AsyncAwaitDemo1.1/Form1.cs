a983b010b98ec1230c0e647ae1966062d0528534
markforest <toxerius7@gmail.com>
2018-03-26 21:09:32 +0300

    finished

---
+++ AsyncAwaitDemo/AsyncAwaitDemo1.1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncAwaitDemo1._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void btnReadFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("Before async call");
+                await GetDataAsync(openFileDialog.FileName);
+                MessageBox.Show("After async call");
+            }
+        }
+
+        private async Task GetDataAsync(string fileName)
+        {
+            byte[] data = null;
+
+            using (FileStream fileStream = File.Open(fileName, FileMode.Open))
+            {
+                data = new byte[fileStream.Length];
+                await fileStream.ReadAsync(data, 0, (int)fileStream.Length);
+            }
+            textBox1.Text = Encoding.Default.GetString(data);
+        }
+    }
+}
---
+++ AsyncAwaitDemo/AsyncAwaitToDataBase/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AsyncAwaitToDataBase
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Data Source =CR4-00\SQLEXPRESS;Initial Catalog=Library;Integrated Security=true;";
+        SqlConnection sqlConnection = null;
+        DataTable dataTable = null;
+        public Form1()
+        { 
+            InitializeComponent();
+            
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            using (sqlConnection = new SqlConnection(connectionString))
+            {
+                await sqlConnection.OpenAsync();
+                string sql = "waitfor delay '00:00:10';";
+                sql += textBox1.Text;
+                SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection);
+                dataTable = new DataTable();
+                SqlDataReader sqlDataReader = await sqlCommand.ExecuteReaderAsync();
+
+                int line = 0;
+                do
+                {
+                    while (await sqlDataReader.ReadAsync())
+                    {
+                        if(line++ == 0) {
+                            for (int i = 0; i < sqlDataReader.FieldCount; i++)
+                            {
+                                dataTable.Columns.Add(sqlDataReader.GetName(i));
+                            }
+                        }
+
+                        DataRow dataRow = dataTable.NewRow();
+                        for (int i = 0; i < sqlDataReader.FieldCount; i++)
+                        {
+                            //dataRow[i] = sqlDataReader[i];
+                            dataRow[i] = await sqlDataReader.GetFieldValueAsync<Object>(i);
+                        }
+                        dataTable.Rows.Add(dataRow);
+                    }
+                } while (await sqlDataReader.NextResultAsync());
+            }
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = dataTable;
+        }
+    }
+}
